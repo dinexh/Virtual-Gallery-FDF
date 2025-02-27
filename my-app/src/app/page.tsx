ae247ac9fbf4f6e5cde55fe3eeaf09f0c31f6a63
Dinesh korukonda <dineshkorukonda05@gmail.com>
2025-02-27 20:07:27 +0530

    created next js app

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="Home-component">
+      <h1>Virtual Art Gallery for Artist</h1>
+      <h3>Developed by 2300030350 - Dinesh Korukonda | Frontend Frameworks Project</h3>
+    </div>
+  )
+}
